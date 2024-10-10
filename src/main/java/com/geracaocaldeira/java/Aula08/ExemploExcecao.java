f4648a5e2b2da5bf6a9d1cf4d82f135381db3ec9
Erika-eni <erikaafedrigo@gmail.com>
2024-10-10 16:36:27 -0300

    Oficial

--- src/main/java/com/geracaocaldeira/java/Aula08/ExemploExcecao.java
+++ src/main/java/com/geracaocaldeira/java/Aula08/ExemploExcecao.java
@@ -1,4 +1,11 @@
 package com.geracaocaldeira.java.Aula08;
 
-public class ExemploExececao {
+public class ExemploExcecao {
+    public boolean isEven(int num){
+        if(num %2 != 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- src/test/java/com/geracaocaldeira/java/Aula08/ExemploExcecaoTests.java
+++ src/test/java/com/geracaocaldeira/java/Aula08/ExemploExcecaoTests.java
@@ -1,4 +1,19 @@
 package com.geracaocaldeira.java.Aula08;
 
-public class ExemploExecacaoTests {
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ExemploExcecaoTests {
+
+    private ExemploExcecao exemplo;
+
+    @Test
+    public void isEvenSucess(){
+        exemplo = new ExemploExcecao();
+        Boolean result = exemplo.isEven(3);
+        Assert.assertTrue(result);
+    }
+
 }
